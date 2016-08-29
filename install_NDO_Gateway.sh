d4d40a3b7e4a008035b4ab1c47cd2d7a0da00a0d
emaintenance <Matthieu-externe.Perrin@edf.fr>
2016-08-29 17:27:06 +0200

    cron install

--- install_NDO_Gateway.sh
+++ install_NDO_Gateway.sh
@@ -4,4 +4,5 @@ mkdir -p /usr/local/scripts
 cp NDO_Gateway.sh /usr/local/scripts/
 chmod +x /usr/local/scripts/*.sh
 cp db_info.conf /etc/db_info.conf
+cat etc_cron.d_ndo_gateway.txt > /etc/cron.d/ndo_gateway
 echo "edit /etc/db_info.conf with database information"
